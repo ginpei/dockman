8d32475380604eb26434f2dd71ebf05761b26185
ginpei <ginpei@gmail.com>
2017-06-05 11:08:42 -0700

    first app

---
+++ main.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const url = require('url');
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+	win = new BrowserWindow({
+		height: 600,
+		width: 800,
+	});
+
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true,
+	}));
+
+	win.webContents.openDevTools();
+
+	win.on('closed', ()=>{
+		win = null;
+	});
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', ()=>{
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
+
+app.on('activate', ()=>{
+	if (!win) {
+		createWindow();
+	}
+});
