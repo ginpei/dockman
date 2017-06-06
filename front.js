6246ebd76565ac13ca65ca73971817613798df50
ginpei <ginpei@gmail.com>
2017-06-06 00:46:41 -0700

    lint

--- front.js
+++ front.js
@@ -2,6 +2,18 @@
 // const Vue = require('vue');
 const { spawn } = require('child_process');
 
+class DockerInfo {
+	constructor(options) {
+		if (options.format) {
+			const fArray = options.format.split('\t');
+			'id image createdAt names status'.split(' ').forEach((key, index)=>{
+				this[key] = fArray[index];
+			});
+		}
+	}
+}
+DockerInfo.format = '{{.ID}}\t{{.Image}}\t{{.CreatedAt}}\t{{.Names}}\t{{.Status}}';
+
 var app = new Vue({
 	el: '#app',
 	data: {
@@ -44,17 +56,5 @@ var app = new Vue({
 		},
 	},
 });
-
-class DockerInfo {
-	constructor(options) {
-		if (options.format) {
-			const fArray = options.format.split('\t');
-			'id image createdAt names status'.split(' ').forEach((key, index)=>{
-				this[key] = fArray[index];
-			});
-		}
-	}
-}
-DockerInfo.format = '{{.ID}}\t{{.Image}}\t{{.CreatedAt}}\t{{.Names}}\t{{.Status}}';
 
 app.update();
