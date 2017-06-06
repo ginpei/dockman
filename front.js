cd487c8e40eb3fa3410e3b10a2dbc304c19346ed
ginpei <ginpei@gmail.com>
2017-06-06 00:39:13 -0700

    remove selected containers

--- front.js
+++ front.js
@@ -7,6 +7,41 @@ var app = new Vue({
 	data: {
 		title: 'Docker Containers',
 		list: [],
+		checked: {},
+	},
+	methods: {
+		update() {
+			const cmd = spawn('docker', ['ps', '-a', '--format', DockerInfo.format]);
+
+			cmd.stdout.on('data', (data)=>{
+				const list = data.toString()
+					.split('\n')
+					.map(d=>new DockerInfo({ format: d }));
+				app.list = list;
+			});
+
+			// TODO: show error and anything needed
+
+			// cmd.stderr.on('data', (data)=>{
+			// 	console.log(`stderr: ${data}`);
+			// });
+
+			// cmd.on('close', (code)=>{
+			// 	console.log(`child process exited with code ${code}`);
+			// });
+		},
+
+		update_onclick(event) {
+			this.update();
+		},
+
+		remove_onclick(event) {
+			const ids = Object.keys(this.checked);
+			const cmd = spawn('docker', ['rm', ids.join(' ')]);
+			cmd.on('close', (code)=>{
+				this.update();
+			});
+		},
 	},
 });
 
@@ -22,21 +57,4 @@ class DockerInfo {
 }
 DockerInfo.format = '{{.ID}}\t{{.Image}}\t{{.CreatedAt}}\t{{.Names}}\t{{.Status}}';
 
-const cmd = spawn('docker', ['ps', '-a', '--format', DockerInfo.format]);
-
-cmd.stdout.on('data', (data)=>{
-	const list = data.toString()
-		.split('\n')
-		.map(d=>new DockerInfo({ format: d }));
-	app.list = list;
-});
-
-// TODO: show error and anything needed
-
-// cmd.stderr.on('data', (data)=>{
-// 	console.log(`stderr: ${data}`);
-// });
-
-// cmd.on('close', (code)=>{
-// 	console.log(`child process exited with code ${code}`);
-// });
+app.update();
