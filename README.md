5febadf7543b3a08e2f3cc8a8212008b04a55def
ginpei <ginpei@gmail.com>
2017-06-14 00:25:53 -0700

    home page with Material Design Lite

---
+++ README.md
@@ -0,0 +1,3 @@
+# Images
+
+- [空港のコンテナを運ぶ車｜ぱくたそフリー写真素材](https://www.pakutaso.com/20110749182post-320.html)
--- front/components/misc/Home.vue
+++ front/components/misc/Home.vue
@@ -1,8 +1,28 @@
 <template>
-	<div>
-		<h1>Dockman</h1>
-		<ul>
-			<li><router-link to="/containers/">Containers</router-link></li>
-		</ul>
+	<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+		<header class="mdl-layout__header">
+			<div class="mdl-layout__header-row">
+				<span class="mdl-layout-title">Dockman</span>
+				<div class="mdl-layout-spacer"></div>
+				<nav class="mdl-navigation mdl-layout--large-screen-only">
+					<router-link to="/containers/" class="mdl-navigation__link">Images</router-link>
+					<router-link to="/containers/" class="mdl-navigation__link">Containers</router-link>
+					<router-link to="/containers/" class="mdl-navigation__link">Volumes</router-link>
+				</nav>
+			</div>
+		</header>
+		<main class="mdl-layout__content">
+		</main>
 	</div>
 </template>
+
+<style scoped>
+main {
+	background-image: url('./images/containers.jpg');
+	background-position: center center;
+	background-size: cover;
+	box-shadow: 0 0 30vw rgba(0,0,0,1) inset;
+	min-height: calc(100vh - 56px);
+	opacity: 0.3;
+}
+</style>
