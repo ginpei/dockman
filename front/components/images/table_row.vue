c7edc096f7b2d68743cb404a91dae4691dfb3a99
ginpei <ginpei@gmail.com>
2017-06-18 22:39:37 -0700

    link to the repository

--- front/components/images/table_row.vue
+++ front/components/images/table_row.vue
@@ -2,8 +2,14 @@
 	<tr :class="classes">
 		<td class="mdl-data-table__cell--non-numeric"><input v-model="$store.state.image.checked[d.id]" type="checkbox" /></td>
 		<td class="mdl-data-table__cell--non-numeric">{{d.id}}</td>
-		<td class="mdl-data-table__cell--non-numeric">{{d.repository}}</td>
-		<td class="mdl-data-table__cell--non-numeric">{{d.tag}}</td>
+		<td v-if="url" class="mdl-data-table__cell--non-numeric">
+			<span @click="repository_click" class="link">{{d.repository}}</span>
+		</td>
+		<td v-else class="mdl-data-table__cell--non-numeric">{{d.repository}}</td>
+		<td v-if="url" class="mdl-data-table__cell--non-numeric">
+			<span @click="tag_click" class="link">{{d.tag}}</span>
+		</td>
+		<td v-else class="mdl-data-table__cell--non-numeric">{{d.tag}}</td>
 		<td class="mdl-data-table__cell--non-numeric">{{d.digest}}</td>
 		<td class="mdl-data-table__cell--non-numeric">{{d.createdSince}}</td>
 		<td class="mdl-data-table__cell--non-numeric">{{d.createdAt}}</td>
@@ -14,5 +20,44 @@
 <script>
 module.exports = {
 	props: ['d'],
+	computed: {
+		url() {
+			let url = null;
+			const name = this.d.repository;
+			const tag = this.d.tag;
+			if (name === '<none>') {
+				// do nothing, it shouldn't be a link
+			}
+			else if (name.includes('/')) {
+				url = `https://hub.docker.com/r/${name}/`;
+			}
+			else if (tag !== 'latest') {
+				url = `https://hub.docker.com/r/library/${name}/`;
+			}
+			return url;
+		},
+
+		taggedUrl() {
+			const name = this.d.repository;
+			const tag = this.d.tag;
+			const url = `https://hub.docker.com/r/library/${name}/tags/${tag}/`;
+			return url;
+		},
+	},
+	methods: {
+		repository_click(event) {
+			const success = window.shell.openItem(this.url);
+			if (!success) {
+				console.error('Failed to open the URL', this.url);
+			}
+		},
+
+		tag_click(event) {
+			const success = window.shell.openItem(this.taggedUrl);
+			if (!success) {
+				console.error('Failed to open the URL', this.taggedUrl);
+			}
+		},
+	},
 };
 </script>
