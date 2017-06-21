8f4d68d2a10f7c856857c922b20557d69d784e8b
ginpei <ginpei@gmail.com>
2017-06-21 01:22:43 -0700

    first design

--- front/components/misc/ErrorMessage.vue
+++ front/components/misc/ErrorMessage.vue
@@ -1,3 +1,13 @@
+<style scoped>
+p {
+	background-color: #fff9f9;
+	border: 1px solid #c99;
+	border-radius: 0.2em;
+	color: #966;
+	padding: 1em;
+}
+</style>
+
 <template>
 	<p>ERROR #{{code}}: <q>{{message}}</q></p>
 </template>
